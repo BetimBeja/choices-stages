6bc23fd708a722a670b7bb8bae18207e75e2262c
asllanmakaj7 <animakaj7@gmail.com>
2024-03-13 16:59:48 +0100

    Updated to Storybook 8

---
+++ sourcecode/.storybook/YourTheme.ts
@@ -0,0 +1,9 @@
+import { create } from "@storybook/theming/create";
+
+export default create({
+  base: "light",
+  brandTitle: "Choices-Stages",
+  brandUrl: "https://github.com/shaheerahmadch/choices-stages",
+  brandImage: "./Shko Online 92x32.svg",
+  brandTarget: "_self",
+});
